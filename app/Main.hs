edc58ecb7d071f16a04101046a4d7724214f8f96
lionitt <lkonontsov@gmail.com>
2017-11-01 13:47:21 +0300

    using scotty

--- app/Main.hs
+++ app/Main.hs
@@ -2,66 +2,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Web.Scotty
+
+import Data.Monoid (mconcat)
+
 import Lib
 
-import Web.Spock
-import Web.Spock.Config
-
-import Data.Aeson       hiding (json)
-import Data.Monoid      ((<>))
-import Data.Text        (Text, pack)
-import GHC.Generics
-
--- data Person = Person
---   { name :: Text
---   , age  :: Int
---   } deriving (Generic, Show)
--- instance ToJSON Person
--- instance FromJSON Person
-
-type Api = SpockM () () () ()
-
-type ApiAction a = SpockAction () () () a
-
-data JsonObject = JsonObject {firstField :: Int, secondField :: Text} deriving(Show, Generic)
-instance ToJSON JsonObject
-instance FromJSON JsonObject
-
-  -- makeChain
-main :: IO ()
+main :: IO()
 main = do
-  spockCfg <- defaultSpockCfg () PCNoDatabase ()
-  runSpock 8080 (spock spockCfg app)
+  scotty 3000 $ do
+  get "/getBlocks/:quantity" $ do
+    quantity <- param "quantity"
+    html $ mconcat ["<h1>Scotty, ", quantity, " me up!</h1>"]
 
-app :: Api
-app = do
-  get "ping" $ do
-    text "  {\"state\": \"ok\", \"answer\": \"pong\"}  "
+  get "/getBlocks" $ do
+    html $ mconcat ["all blocks"]
 
-  get ("fuck" <//> var) $ \name -> do
-    text $ "Fuck you, " <> name <> "!"
-
-  post "ppc" $ do 
-    body <- jsonBody' :: ApiAction JsonObject
-    text $ "ich habe win" <> pack (show body)
-
-  -- post ("send" <//> var) $ \value -> do
-
-  -- get ("take" <//> var) $ \howmany -> do
-    
-
-    -- "hello f world"
-
--- main :: IO ()
--- main = someFunc
-
-
-{-
-
-GET  /ping                 - sends pong
-GET  /take?[howmany=[0..]] - give your blocks
-GET  /nodes                - give your nodes
-
-POST /send?value={sth}     - sends string to block-monad
-
--}
+  post "/post" $ do
+    string <- param "value"
+    html $ mconcat ["kjdgf", string, "yetr"]
+  -- putStrLn
