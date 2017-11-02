fd4da1dbeba554025696a47cc18d8b64a4e2f3f5
lionitt <lkonontsov@gmail.com>
2017-11-02 18:03:47 +0300

    Impl node and node startup script

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
+import           System.Environment (getArgs)
 import qualified Control.Distributed.Backend.P2P as P2P
 import           Control.Monad.Trans (liftIO)
+import           Control.Monad (forever)
 import           Control.Concurrent (threadDelay)
 import Control.Distributed.Process.Node (initRemoteTable)
 
 main = do
-  P2P.bootstrap "127.0.0.1" "9001" [P2P.makeNodeId "seedhost:9000"] initRemoteTable $ do
-    liftIO $ threadDelay 1000000000 -- give dispatcher a second to discover other nodes
-    P2P.nsendPeers "myService" ("some", "message")
+  [from, to] <- getArgs
+  P2P.bootstrap "127.0.0.1" from 
+    [P2P.makeNodeId ("127.0.0.1:" ++ to)
+    -- , P2P.makeNodeId "127.0.0.1:8999"
+    -- , P2P.makeNodeId "127.0.0.1:8888"
+    ] initRemoteTable $ do
+    liftIO $ threadDelay 100000000 -- give dispatcher a second to discover other nodes
+    P2P.nsendPeers "pidor" ("bonan", "tagon"
