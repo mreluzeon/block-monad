9d0ab57fa9bba15b84d26070a73b5a895e154d9a
lionitt <lkonontsov@gmail.com>
2017-11-03 11:13:20 +0300

    senchopens pochinil wifi

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,12 @@
 module Main where
 
 import           System.Environment (getArgs)
+import           Control.Distributed.Process
 import qualified Control.Distributed.Backend.P2P as P2P
 import           Control.Monad.Trans (liftIO)
 import           Control.Monad (forever)
 import           Control.Concurrent (threadDelay)
-import Control.Distributed.Process.Node (initRemoteTable)
+import           Control.Distributed.Process.Node (initRemoteTable)
 
 main = do
   [from, to] <- getArgs
@@ -15,5 +16,18 @@ main = do
     -- , P2P.makeNodeId "127.0.0.1:8999"
     -- , P2P.makeNodeId "127.0.0.1:8888"
     ] initRemoteTable $ do
-    liftIO $ threadDelay 100000000 -- give dispatcher a second to discover other nodes
-    P2P.nsendPeers "pidor" ("bonan", "tagon"
+    liftIO $ threadDelay 1000000 -- give dispatcher a second to discover other nodes
+    P2P.nsendPeers ("dver " ++ from) "H - водород"
+    -- say "sth"
+    P2P.nsendPeers ("dver " ++ from) "H - водород"
+    P2P.nsendPeers ("dver " ++ from) "H - водород"
+    P2P.nsendPeers ("dver " ++ from) "H - водород"
+    forever $ do
+      sth <- expect :: Process String
+      say sth
+      -- liftIO $ putStrLn "pidSkaz"
+    -- liftIO $ threadDelay 1000000
+    -- sth <- expect :: Process String
+    -- liftIO $ putStrLn "pidor skazal: "
+
+-- monabλock
