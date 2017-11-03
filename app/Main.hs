79ba924c681f4a7cdec69a3b0f520509af97061b
Sencho Pens <senya@chekanov.net>
2017-11-03 13:01:25 +0300

    Impl message sharing

--- app/Main.hs
+++ app/Main.hs
@@ -13,21 +13,24 @@ main = do
   [from, to] <- getArgs
   P2P.bootstrap "127.0.0.1" from 
     [P2P.makeNodeId ("127.0.0.1:" ++ to)
-    -- , P2P.makeNodeId "127.0.0.1:8999"
-    -- , P2P.makeNodeId "127.0.0.1:8888"
     ] initRemoteTable $ do
     liftIO $ threadDelay 1000000 -- give dispatcher a second to discover other nodes
-    P2P.nsendPeers ("dver " ++ from) "H - водород"
-    -- say "sth"
-    P2P.nsendPeers ("dver " ++ from) "H - водород"
-    P2P.nsendPeers ("dver " ++ from) "H - водород"
-    P2P.nsendPeers ("dver " ++ from) "H - водород"
+
+    self <- getSelfPid
+    say $ "Me: " ++ show self
+
+    let myName = "dver"
+    register myName self
+    say "Registered!!!"
+
+    P2P.nsendPeers myName "H - водород"
+
+    peers <- P2P.getPeers
+    say $ show peers
+
     forever $ do
-      sth <- expect :: Process String
-      say sth
-      -- liftIO $ putStrLn "pidSkaz"
-    -- liftIO $ threadDelay 1000000
-    -- sth <- expect :: Process String
-    -- liftIO $ putStrLn "pidor skazal: "
+      say "Waiting... <<<<<<<<"
+      receiveWait [ matchAny (\m -> say "asjf;sadjfk dsa;djf") ]
+      say "ASDFSDFSDF"
 
 -- monabλock
