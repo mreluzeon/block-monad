fd2f4753ddeed85619dc945f2be77cba33ccda4d
Sencho Pens <senya@chekanov.net>
2017-11-04 11:43:53 +0300

    Impl rpc

--- app/Main.hs
+++ app/Main.hs
@@ -2,34 +2,67 @@
 module Main where
 
 import           System.Environment (getArgs)
+import           Control.Concurrent (forkIO)
 
+import           Control.Concurrent.STM
 import           Control.Distributed.Process
 import qualified Control.Distributed.Backend.P2P as P2P
 import           Control.Monad.Trans (liftIO)
-import           Control.Monad (forever)
+import           System.IO (isEOF)
+import           Control.Monad (forever, unless)
 import           Control.Concurrent (threadDelay)
 import           Control.Distributed.Process.Node (initRemoteTable)
 
+import FlowerMap
+import Motivators
+
+stateService = "bees"
+
+shareState :: FlowerMap -> Process()
+shareState flowerMap = do
+  flowers <- liftIO $ atomically $ getCoordinates flowerMap
+  P2P.nsendPeers stateService flowers
+
+updateState :: FlowerMap -> Coordinates -> Process ()
+updateState flowerMap newFlowers = do
+  liftIO $ atomically $ flowerMap `addCoordinates` newFlowers
+  myFlowers <- liftIO $ atomically $ getCoordinates flowerMap 
+  say $ show myFlowers
+
+waitInput :: FlowerMap -> IO ()
+waitInput flowerMap = do
+  done <- isEOF
+  unless done $ do
+    line <- getLine
+    let coord = read line :: Coordinate
+    atomically $ do
+      flowerMap `addCoordinate` coord
+    waitInput flowerMap
 
 main = do
   [from, to] <- getArgs
-  P2P.bootstrap "127.0.0.1" from 
-    [P2P.makeNodeId ("127.0.0.1:" ++ to)
-    ] initRemoteTable $ do
+  flowerMap <- atomically makeMap
+  P2P.bootstrap "127.0.0.1" from [P2P.makeNodeId ("127.0.0.1:" ++ to)] initRemoteTable $ do
     liftIO $ threadDelay 1000000 -- give dispatcher a second to discover other nodes
 
     self <- getSelfPid
     say $ "Me: " ++ show self
 
-    let myName = "dver"
-    register myName self
-    say "Registered!!!"
+    register stateService self
+    say "Registered"
+    liftIO $ threadDelay 3000000 -- give other nodes time to register
 
-    P2P.nsendPeers myName "H - водород"
+    P2P.nsendPeers stateService $ "Hi! My port is " ++ (show from)
 
+    spawnLocal $ shareStateMotivator self
+    liftIO $ forkIO $ waitInput flowerMap
+
+    let actionHandler :: Action -> Process ()
+        actionHandler _ = shareState flowerMap
     forever $ do
-      say "Waiting... <<<<<<<<"
-      receiveWait [ matchAny (\m -> say "asjf;sadjfk dsa;djf") ]
-      say "ASDFSDFSDF"
+      --receiveWait [ match (\(_ :: Action) -> shareState self flowerMap)
+      receiveWait [ match actionHandler
+                  , match $ updateState flowerMap
+                  ]
 
 -- monabλock
