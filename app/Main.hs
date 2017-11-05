cd38d5e1fab659fdfdf79bf0ef294425ecb4e792
Sencho Pens <senya@chekanov.net>
2017-11-05 03:51:24 +0300

    feat: WORKING CRYPTO MAZAFAKA

--- app/Main.hs
+++ app/Main.hs
@@ -1,73 +1,71 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-import           System.Environment (getArgs)
-import           Control.Concurrent (forkIO)
+import System.Environment (getArgs)
+import Control.Concurrent (forkIO)
 
-import           Control.Concurrent.STM
-import           Control.Distributed.Process
+import Control.Concurrent.STM
+import Control.Distributed.Process
 import qualified Control.Distributed.Backend.P2P as P2P
-import           Control.Monad.Trans (liftIO)
-import           System.IO (isEOF)
-import           Control.Monad (forever, unless)
-import           Control.Concurrent (threadDelay)
-import           Control.Distributed.Process.Node (initRemoteTable)
-
-import FlowerMap
-import Motivators
-import Lib
-import Api
+import Control.Monad.Trans (liftIO)
+import System.IO (isEOF)
+import Control.Monad (forever, unless)
+import Control.Concurrent (threadDelay)
+import Control.Distributed.Process.Node (initRemoteTable)
 import Network.Wai
 import Network.Wai.Handler.Warp
+
+import FlowerMap
+import STMSet
+import Motivators
+import Crypto
+import Api
+import States
 --import Client
 
+type Flower = (Int, Int)
+type FlowerMap = STMSet Flower
+type FlowerState = State Flower
 
-stateService = "bees"
-
-shareState :: FlowerMap -> Process()
-shareState flowerMap = do
-  flowers <- liftIO $ atomically $ getCoordinates flowerMap
-  P2P.nsendPeers stateService flowers
-
-updateState :: FlowerMap -> Coordinates -> Process ()
-updateState flowerMap newFlowers = do
-  liftIO $ atomically $ flowerMap `addCoordinates` newFlowers
-  myFlowers <- liftIO $ atomically $ getCoordinates flowerMap
-  say $ show myFlowers
-
-waitInput :: FlowerMap -> IO ()
-waitInput flowerMap = do
+waitInput :: (Ord a, Read a) => STMSet a -> IO ()
+waitInput set = do
   done <- isEOF
   unless done $ do
     line <- getLine
-    let coord = read line :: Coordinate
+    let elem = read line
     atomically $ do
-      flowerMap `addCoordinate` coord
-    waitInput flowerMap
+      set `addElem` Elem elem
+    waitInput set
 
 main = do
   [from, to] <- getArgs
-  flowerMap <- atomically makeMap
+
+  flowerMap <- atomically makeSet
+  let flowerState = State{state=flowerMap, serviceName="bees"} :: FlowerState
+
+  keys <- newKeyPair
+
   print "Launching server"
-  forkIO $ run 8000 $ app flowerMap
+  --forkIO $ run 8000 $ app flowerMap
   print "Launched!"
+
   P2P.bootstrap "127.0.0.1" from [P2P.makeNodeId ("127.0.0.1:" ++ to)] initRemoteTable $ do
-    liftIO $ threadDelay 1000000 -- give dispatcher a second to discover other nodes
+    liftIO $ threadDelay 3000000 -- give dispatcher a second (or 2) to discover other nodes
 
     self <- getSelfPid
     say $ "Me: " ++ show self
 
-    register stateService self
+    registerStates self [flowerState]
     say "Registered"
     liftIO $ threadDelay 3000000 -- give other nodes time to register
 
-    P2P.nsendPeers stateService $ "Hi! My port is " ++ (show from)
+    --P2P.nsendPeers stateService $ "Hi! My port is " ++ (show from)
 
     spawnLocal $ shareStateMotivator self
     liftIO $ forkIO $ waitInput flowerMap
 
     let actionHandler :: Action -> Process ()
-        actionHandler _ = shareState flowerMap
+        actionHandler _ = shareState keys flowerState
     forever $ do
       --receiveWait [ match (\(_ :: Action) -> shareState self flowerMap)
       receiveWait [ match actionHandler
--- src/Crypto.hs
+++ src/Crypto.hs
@@ -4,8 +4,12 @@
 
 module Crypto
   ( newKeyPair
+  , KeyPair
   , sign
   , verify
+  , signMsg
+  , verifyMsg
+  , SignedMessage(..)
   ) where
 
 import Crypto.Hash
@@ -13,6 +17,7 @@ import qualified Crypto.PubKey.ECC.ECDSA as ECDSA
 import qualified Crypto.PubKey.ECC.Generate as ECC
 import qualified Crypto.PubKey.ECC.Types as ECC
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import Data.Binary
 import GHC.Generics (Generic)
 import Data.Typeable
@@ -44,7 +49,7 @@ type KeyPair = (ECDSA.PublicKey, ECDSA.PrivateKey)
 
 data SignedMessage a = SignedMessage { signature :: ECDSA.Signature
                                      , msg :: a
-                                     , pk :: ECDSA.PublicKey
+                                     , pubk :: ECDSA.PublicKey
                                      } deriving (Show,Read,Eq,Data,Generic,Typeable)
 
 instance (Binary a) => Binary (SignedMessage a)
@@ -58,5 +63,11 @@ newKeyPair = ECC.generate sec_p256k1
 sign :: ECDSA.PrivateKey -> B.ByteString -> IO ECDSA.Signature
 sign pk = ECDSA.sign pk SHA3_256
 
+signMsg :: (Binary a) => ECDSA.PrivateKey -> a -> IO ECDSA.Signature
+signMsg pk msg = ECDSA.sign pk SHA3_256 $ BL.toStrict $ encode msg
+
 verify :: ECDSA.PublicKey -> ECDSA.Signature -> B.ByteString -> Bool
 verify = ECDSA.verify SHA3_256
+
+verifyMsg :: (Binary a) => ECDSA.PublicKey -> ECDSA.Signature -> a -> Bool
+verifyMsg pubk sig msg = verify pubk sig $ BL.toStrict $ encode msg
